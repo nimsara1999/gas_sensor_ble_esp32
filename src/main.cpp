ff3bb0251f640bf1dedbcc110a51633e2ea98147
sasindu-wijesuriya <sasindu.20@cse.mrt.ac.lk>
2024-09-05 04:53:58 +0530

    Add BLE functionality and handle gas sensor data

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,129 @@
 #include <ArduinoJson.h>
 #include <EEPROM.h>
 #include <HTTPClient.h>
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEScan.h>
+#include <BLEAdvertisedDevice.h>
 
 const char *ap_ssid = "ESP32-AP";
 const char *ap_password = "123456789";
+const int scanTimeSeconds = 1;
 
+const int BOOT_PIN = 0;
+const int LED_PIN = 2;
+bool inAPMode = false;
+unsigned long previousMillis = 0;
+const long interval = 500;
+
+BLEScan *pBLEScan;
 WebServer server(80);
 
 const int SSID_ADDR = 0;
 const int PASS_ADDR = 50;
 const int EEPROM_SIZE = 512;
+
+std::string string_to_hex(const std::string &input)
+{
+  static const char hex_digits[] = "0123456789abcdef";
+  std::string output;
+  output.reserve(input.length() * 2);
+  for (unsigned char c : input)
+  {
+    output.push_back(hex_digits[c >> 4]);
+    output.push_back(hex_digits[c & 15]);
+  }
+  return output;
+}
+
+std::string format_hex_string(const std::string &hexString)
+{
+  std::string formattedString;
+  for (size_t i = 0; i < hexString.length(); i += 2)
+  {
+    formattedString += hexString.substr(i, 2);
+    if (i + 2 < hexString.length())
+    {
+      formattedString += " ";
+    }
+  }
+  return formattedString;
+}
+
+int hex_to_int(const std::string &hexString)
+{
+  return strtol(hexString.c_str(), nullptr, 16);
+}
+
+class AdvertisedDeviceCallbacks : public BLEAdvertisedDeviceCallbacks
+{
+  void onResult(BLEAdvertisedDevice advertisedDevice)
+  {
+    if (strcmp(advertisedDevice.getName().c_str(), "") == 0)
+    {
+      std::string hexAdvData = string_to_hex(advertisedDevice.getManufacturerData());
+      if (hexAdvData.rfind("544e", 0) == 0)
+      {
+        std::string frameHead1 = hexAdvData.substr(0, 4);
+        std::string type = hexAdvData.substr(4, 2);
+        std::string cmd = hexAdvData.substr(6, 2);
+        std::string frameHead2 = hexAdvData.substr(8, 4);
+        std::string measurementHex = hexAdvData.substr(12, 4);
+        std::string batteryHex = hexAdvData.substr(16, 2);
+
+        int measurement = hex_to_int(measurementHex);
+        int battery = hex_to_int(batteryHex);
+
+        Serial.printf("*********************\n");
+        Serial.printf("Received Data: %s\n", format_hex_string(hexAdvData).c_str());
+        Serial.printf("Frame Head: %s\n", frameHead1.c_str());
+        Serial.printf("Type: %s\n", type.c_str());
+        Serial.printf("Cmd: %s\n", cmd.c_str());
+        Serial.printf("Measurement Result (US): %d\n", measurement);
+        Serial.printf("Battery: %d % \n", battery);
+        Serial.printf("RSSI: %d\n", advertisedDevice.getRSSI());
+        Serial.printf("Prepared packet: \n");
+        Serial.printf("{\"DATETIME\":1724343139511,\"IMEI\":\"A4C138CCD9ED\",\"NCU_FW_VER\":109,\"GAS_METER\":%d,\"CSQ\":104,\"MCU_TEMP\":30,\"BAT_VOL\":%d,\"METER_TYPE\":4,\"TIME_ZONE\":\"+07\",\"TANK_SIZE\":\"20lb\",\"GAS_PERCENT\":0}\n\n", measurement, battery);
+      }
+    }
+  }
+};
+
+void switchToAPMode()
+{
+  Serial.println("Switching to AP mode...");
+  WiFi.mode(WIFI_AP);
+  WiFi.softAP(ap_ssid, ap_password);
+  Serial.println("Access Point Started");
+  Serial.print("AP IP Address: ");
+  Serial.println(WiFi.softAPIP());
+}
+
+void blinkLEDInAPMode()
+{
+  if (inAPMode)
+  {
+    unsigned long currentMillis = millis();
+    if (currentMillis - previousMillis >= interval)
+    {
+      previousMillis = currentMillis;
+      int state = digitalRead(LED_PIN);
+      digitalWrite(LED_PIN, !state);
+    }
+  }
+}
+
+void handleButtonPress()
+{
+  if (digitalRead(BOOT_PIN) == LOW)
+  {
+    delay(100);
+    if (digitalRead(BOOT_PIN) == LOW)
+    {
+      switchToAPMode();
+    }
+  }
+}
 
 void saveWiFiCredentials(const String &ssid, const String &password)
 {
@@ -81,7 +195,7 @@ bool tryConnectToSavedWiFi()
   return false;
 }
 
-void check_internet_connection()
+void handle_check_internet_connection()
 {
   if (server.method() == HTTP_GET)
   {
@@ -112,7 +226,15 @@ void check_internet_connection()
   }
 }
 
-void handleSend()
+void indicateSuccessfulConnection()
+{
+  digitalWrite(LED_PIN, HIGH); // Turn the LED on
+  delay(3000);                 // Keep the LED on for 3 seconds
+  digitalWrite(LED_PIN, LOW);  // Turn the LED off
+  inAPMode = false;            // Exit AP mode, stop blinking
+}
+
+void handle_connect_to_new_wifi()
 {
   if (server.method() == HTTP_POST)
   {
@@ -163,6 +285,8 @@ void handleSend()
       Serial.println(WiFi.localIP());
       Serial.print("Wi-Fi server (AP) IP Address: ");
       Serial.println(WiFi.softAPIP());
+
+      indicateSuccessfulConnection();
     }
     else
     {
@@ -185,6 +309,10 @@ void setup()
 
   EEPROM.begin(EEPROM_SIZE);
 
+  pinMode(BOOT_PIN, INPUT_PULLUP);
+  pinMode(LED_PIN, OUTPUT);
+  digitalWrite(LED_PIN, LOW);
+
   // Try to connect to saved Wi-Fi credentials
   if (!tryConnectToSavedWiFi())
   {
@@ -196,9 +324,17 @@ void setup()
     Serial.print("AP IP Address: ");
     Serial.println(WiFi.softAPIP());
   }
+  Serial.println("Scanning for Gas sensors...");
 
-  server.on("/connect-to-new-wifi", HTTP_POST, handleSend);
-  server.on("/check-internet", HTTP_GET, check_internet_connection);
+  BLEDevice::init("");
+  pBLEScan = BLEDevice::getScan();
+  pBLEScan->setAdvertisedDeviceCallbacks(new AdvertisedDeviceCallbacks());
+  pBLEScan->setActiveScan(false);
+  pBLEScan->setInterval(100);
+  pBLEScan->setWindow(99);
+
+  server.on("/connect-to-new-wifi", HTTP_POST, handle_connect_to_new_wifi);
+  server.on("/check-internet", HTTP_GET, handle_check_internet_connection);
 
   server.begin();
   Serial.println("HTTP server started");
@@ -207,4 +343,10 @@ void setup()
 void loop()
 {
   server.handleClient();
+
+  handleButtonPress();
+  blinkLEDInAPMode();
+
+  BLEScanResults foundDevices = pBLEScan->start(scanTimeSeconds, false);
+  pBLEScan->clearResults();
 }
